082335f0cadbb338f0ed1f72a212645c913e040f
Brototi Biswas <72146916+brototibiswas@users.noreply.github.com>
2023-06-28 19:42:12 -0400

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Brototi Biswas on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
