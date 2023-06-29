008d3275abaff6a378bce565b952e55ab56351b3
Brototi Biswas <72146916+brototibiswas@users.noreply.github.com>
2023-06-28 20:39:16 -0400

    Setup Project

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Brototi Biswas on 6/28/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("IconGeneral")
+    static let notificationIcon = Color("Notification")
+    static let text = Color("Text")
+    static let containerColor = Color("Container")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
